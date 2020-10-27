c1145192773ae3fea4c0580d4528341a8b446e30
tranhien <tuonglaisau50@gmail.com>
2020-10-27 17:01:42 +0700

    add repository send sms

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace SpeedSMSAPI
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+            SpeedSMSAPI api = new SpeedSMSAPI("S2ejUPVthW3rN4AwwMuHeRRYDjGxXH9W");
+			
+			String phones = "0342461010" ;
+            String str = "5558";
+            String response = api.VerifyCode(phones, str);
+			Console.WriteLine(response);
+			Console.ReadLine();
+        }
+    }
+}
